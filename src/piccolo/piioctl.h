a319fbc2fa9983a53773a95b65568d391827d81c
coloroftears <coloroftears88@gmail.com>
2016-11-18 04:06:14 +0000

    Reformat (style=Chromium) src/piccolo/piioctl.h

--- src/piccolo/piioctl.h
+++ src/piccolo/piioctl.h
@@ -15,37 +15,34 @@
 // The IOCTL function codes from 0x800 to 0xFFF are for customer use.
 
 #define IOCTL_ROMEO_OPN3_WRITE_UCHAR \
-    CTL_CODE( ROMEO_TYPE, 0x900, METHOD_BUFFERED, FILE_WRITE_ACCESS )
+  CTL_CODE(ROMEO_TYPE, 0x900, METHOD_BUFFERED, FILE_WRITE_ACCESS)
 
 #define IOCTL_ROMEO_OPN3_READ_UCHAR \
-    CTL_CODE( ROMEO_TYPE, 0x901, METHOD_BUFFERED, FILE_READ_ACCESS )
+  CTL_CODE(ROMEO_TYPE, 0x901, METHOD_BUFFERED, FILE_READ_ACCESS)
 
 #define IOCTL_ROMEO_OPN3_SETREG \
-    CTL_CODE( ROMEO_TYPE, 0x902, METHOD_BUFFERED, FILE_WRITE_ACCESS )
+  CTL_CODE(ROMEO_TYPE, 0x902, METHOD_BUFFERED, FILE_WRITE_ACCESS)
 
 #define IOCTL_ROMEO_OPN3_RESET \
-    CTL_CODE( ROMEO_TYPE, 0x903, METHOD_BUFFERED, FILE_WRITE_ACCESS )
+  CTL_CODE(ROMEO_TYPE, 0x903, METHOD_BUFFERED, FILE_WRITE_ACCESS)
 
-typedef struct  _ROMEO_WRITE_INPUT 
-{
-    ULONG   PortNumber;     // Port # to write to
-    union   {               // Data to be output to port
-        ULONG   LongData;
-        USHORT  ShortData;
-        UCHAR   CharData;
-    };
-}   ROMEO_WRITE_INPUT, *PROMEO_WRITE_INPUT;
+typedef struct _ROMEO_WRITE_INPUT {
+  ULONG PortNumber;  // Port # to write to
+  union {            // Data to be output to port
+    ULONG LongData;
+    USHORT ShortData;
+    UCHAR CharData;
+  };
+} ROMEO_WRITE_INPUT, *PROMEO_WRITE_INPUT;
 
-typedef struct ROMEO_WRITEREG_t
-{
-    UCHAR   code;
-    UCHAR   data;
-    USHORT  port;
+typedef struct ROMEO_WRITEREG_t {
+  UCHAR code;
+  UCHAR data;
+  USHORT port;
 } ROMEO_WRITEREG;
 
-#define PICCOLO_WRITE       0x00
-#define PICCOLO_WAITTIMER   0x01
+#define PICCOLO_WRITE 0x00
+#define PICCOLO_WAITTIMER 0x01
 
 #define IOCTL_ROMEO_OPN3_WRITE \
-    CTL_CODE( ROMEO_TYPE, 0x904, METHOD_BUFFERED, FILE_WRITE_ACCESS )
-
+  CTL_CODE(ROMEO_TYPE, 0x904, METHOD_BUFFERED, FILE_WRITE_ACCESS)
