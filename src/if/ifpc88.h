577587f4e06c1e704c79fd7bb69f7eb529c4c22f
coloroftears <coloroftears88@gmail.com>
2016-11-18 04:04:46 +0000

    Reformat (style=Chromium) src/if

--- src/if/ifpc88.h
+++ src/if/ifpc88.h
@@ -1,10 +1,9 @@
 #ifndef incl_interface_pc88_h
 #define incl_interface_pc88_h
 
-struct IDMAAccess
-{
-    virtual uint IFCALL RequestRead (uint bank, uint8* data, uint nbytes) = 0;
-    virtual uint IFCALL RequestWrite(uint bank, uint8* data, uint nbytes) = 0;
+struct IDMAAccess {
+  virtual uint IFCALL RequestRead(uint bank, uint8* data, uint nbytes) = 0;
+  virtual uint IFCALL RequestWrite(uint bank, uint8* data, uint nbytes) = 0;
 };
 
-#endif // incl_interface_pc88_h
+#endif  // incl_interface_pc88_h
--- src/if/ifui.h
+++ src/if/ifui.h
@@ -13,24 +13,19 @@
 #define IFCALL __stdcall
 #endif
 
-interface IMouseUI : public IUnk
-{
-    virtual bool IFCALL Enable(bool en) = 0;
-    virtual bool IFCALL GetMovement(POINT*) = 0;
-    virtual uint IFCALL GetButton() = 0;
+interface IMouseUI : public IUnk {
+  virtual bool IFCALL Enable(bool en) = 0;
+  virtual bool IFCALL GetMovement(POINT*) = 0;
+  virtual uint IFCALL GetButton() = 0;
 };
 
-
-
-struct PadState
-{
-    uint8 direction;        // b0:Å™ b1:Å´ b2:Å© b3:Å®  active high
-    uint8 button;           // b0-3, active high
+struct PadState {
+  uint8 direction;  // b0:Å™ b1:Å´ b2:Å© b3:Å®  active high
+  uint8 button;     // b0-3, active high
 };
 
-interface IPadInput 
-{
-    virtual void IFCALL GetState(PadState*) = 0;
+interface IPadInput {
+  virtual void IFCALL GetState(PadState*) = 0;
 };
 
-#endif 
+#endif
